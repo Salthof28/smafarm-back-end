4f06ca002e98d8f4e466e0d2fa443cce54616aac
Salman <salmanalthof31@gmail.com>
2025-08-12 00:13:35 +0700

    refactor: create dto for livestock

--- src/livestocks/dto/req/create-livestock.dto.ts
+++ src/livestocks/dto/req/create-livestock.dto.ts
@@ -1,1 +1,23 @@
-export class CreateLivestockDto {}
+import { Prisma } from "@prisma/client";
+import { IsArray, IsNumber, IsOptional, IsString } from "class-validator";
+
+export class CreateLivestockDto {
+    @IsNumber()
+    category_id: number;
+    @IsString()
+    title: string;
+    @IsNumber()
+    age: number;
+    @IsNumber()
+    price: Prisma.Decimal;
+    @IsNumber()
+    stock: number;
+    @IsString()
+    description: string
+    @IsString()
+    location: string;
+    @IsOptional()
+    @IsArray()
+    @IsString({ each: true })
+    img_livestock?: string[];
+}
---
+++ src/livestocks/dto/res/livestock-body.dto.ts
@@ -0,0 +1,36 @@
+import { Expose, Type } from "class-transformer";
+
+export class LivestockBodyDto {
+    @Expose()
+    @Type(() => Number)
+    id: number
+    @Expose()
+    @Type(() => Number)
+    category_id: number
+    @Expose()
+    @Type(() => Number)
+    farm_id: number
+    @Expose()
+    @Type(() => String)
+    title: string;
+    @Expose()
+    @Type(() => Number)
+    age: number;
+    @Expose()
+    @Type(() => Number)
+    price: number;
+    @Expose()
+    @Type(() => Number)
+    stock: number;
+    @Expose()
+    @Type(() => String)
+    description: string;
+    @Expose()
+    @Type(() => String)
+    location: string;
+    @Type(() => Date)
+    created_at: Date
+    @Expose()
+    @Type(() => Date)
+    updated_at: Date
+}
